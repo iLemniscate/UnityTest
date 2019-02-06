aa46cf4d98f6aeaf2f8eb4e85cf28e6a16690293
Ubassy Abdillah <elemniscate@gmail.com>
2019-02-06 14:37:50 +0700

    Add Project

---
+++ Unity Test/Assets/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float spawnRate = 1f;
+    public GameObject hexagon;
+    float nextTimeToSpawn = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextTimeToSpawn)
+        {
+            Instantiate(hexagon, Vector3.zero, Quaternion.identity);
+            nextTimeToSpawn = Time.time + 1f / spawnRate;
+        }
+    }
+}
